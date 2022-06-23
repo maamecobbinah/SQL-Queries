f67152f54fa5643ec41e0df361c5d637e0a26a88
Maame Cobbinah <102822534+maamecobbinah@users.noreply.github.com>
2022-06-23 16:32:53 -0400

    Update Exercises.sql

--- Exercises.sql
+++ Exercises.sql
@@ -68,3 +68,58 @@ SELECT FirstName,
 FROM AdventureWorks2019.Person.Person t1
 JOIN AdventureWorks2019.HumanResources.Employee t2 on t1.BusinessEntityID = t2.BusinessEntityID
 JOIN AdventureWorks2019.HumanResources.EmployeePayHistory t3 on t1.BusinessEntityID = t3.BusinessEntityID
+
+---Partition By 
+---Create a query with the following columns:
+---“Name” from the Production.Product table, which can be alised as “ProductName”
+---“ListPrice” from the Production.Product table
+---“Name” from the Production. ProductSubcategory table, which can be alised as “ProductSubcategory”*
+---“Name” from the Production. Category table, which can be alised as “ProductCategory”** 
+SELECT
+t1.Name AS ProductName,
+ListPrice,
+t2.Name AS ProductSubcategory,
+t3.Name AS ProductCategory
+FROM AdventureWorks2019.Production.Product t1
+JOIN AdventureWorks2019.Production.ProductSubcategory t2  ON t1.ProductSubcategoryID = t2.ProductSubcategoryID
+JOIN AdventureWorks2019.Production.ProductCategory t3  ON t1.ProductSubcategoryID = t3.ProductCategoryID
+
+--Enhance your query from above by adding a derived column called
+--"AvgPriceByCategory " that returns the average ListPrice for the product category in each given row
+SELECT
+t1.Name AS ProductName,
+ListPrice,
+t2.Name AS ProductSubcategory,
+t3.Name AS ProductCategory,
+AVG(ListPrice) OVER(PARTITION BY t3.Name) AS AvgPriceByCategory
+FROM AdventureWorks2019.Production.Product t1
+JOIN AdventureWorks2019.Production.ProductSubcategory t2  ON t1.ProductSubcategoryID = t2.ProductSubcategoryID
+JOIN AdventureWorks2019.Production.ProductCategory t3  ON t2.ProductSubcategoryID = t3.ProductCategoryID
+
+---Enhance your query from above by adding a derived column called
+---"AvgPriceByCategoryAndSubcategory" that returns the average ListPrice for the product category AND subcategory in each given row.
+SELECT
+t1.Name AS ProductName,
+ListPrice,
+t2.Name AS ProductSubcategory,
+t3.Name AS ProductCategory,
+AVG(ListPrice) OVER(PARTITION BY t3.Name) AS AvgPriceByCategory,
+AVG(ListPrice) OVER(PARTITION BY t3.Name, t2.Name) AS AvgPriceByCategoryAndSubcategory
+FROM AdventureWorks2019.Production.Product t1
+JOIN AdventureWorks2019.Production.ProductSubcategory t2  ON t1.ProductSubcategoryID = t2.ProductSubcategoryID
+JOIN AdventureWorks2019.Production.ProductCategory t3  ON t2.ProductSubcategoryID = t3.ProductCategoryID
+
+---Enhance your query from above by adding a derived column called
+---"ProductVsCategoryDelta" that returns the result of the following calculation:
+---A product's list price, MINUS the average ListPrice for that product’s category.
+SELECT
+t1.Name AS ProductName,
+ListPrice,
+t2.Name AS ProductSubcategory,
+t3.Name AS ProductCategory,
+AVG(ListPrice) OVER(PARTITION BY t3.Name) AS AvgPriceByCategory,
+AVG(ListPrice) OVER(PARTITION BY t3.Name, t2.Name) AS AvgPriceByCategoryAndSubcategory,
+ListPrice - AVG(ListPrice) OVER(PARTITION BY t3.Name) AS AvgPriceByCategory
+FROM AdventureWorks2019.Production.Product t1
+JOIN AdventureWorks2019.Production.ProductSubcategory t2  ON t1.ProductSubcategoryID = t2.ProductSubcategoryID
+JOIN AdventureWorks2019.Production.ProductCategory t3  ON t2.ProductSubcategoryID = t3.ProductCategoryID
