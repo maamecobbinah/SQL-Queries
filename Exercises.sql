3f5f63dbb05ce65a814d3e8a36919d79dd1ff162
Maame Cobbinah <102822534+maamecobbinah@users.noreply.github.com>
2022-07-04 00:33:26 -0400

    Update Exercises.sql

--- Exercises.sql
+++ Exercises.sql
@@ -306,3 +306,20 @@ PIVOT(
 AVG(VacationHours)
 FOR JobTitle IN ([Sales Representative],[Buyer],[Janitor]) 
 )B 
+
+----Use a recursive CTE to generate a list of all odd numbers between 1 and 100.
+WITH OddNumber AS (SELECT 1 AS MyNumber
+UNION ALL
+SELECT MyNumber + 2
+FROM OddNumber
+WHERE MyNumber < 99)
+SELECT MyNumber FROM OddNumber
+
+---Use a recursive CTE to generate a date series of all FIRST days of the month (1/1/2021, 2/1/2021, etc.)
+--from 1/1/2020 to 12/1/2029.
+WITH DateSeries AS (SELECT CAST('01-01-2021' AS DATE) AS MyDate
+UNION ALL
+SELECT DATEADD(MONTH, 1, MyDate)
+FROM DateSeries
+WHERE MyDate <  CAST('12-01-2029' AS DATE))
+SELECT MyDate FROM DateSeries  OPTION(MAXRECURSION 365)
