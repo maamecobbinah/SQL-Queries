ada0c8743c439145d709b6ab0ef1a3b01b0ca1e4
Maame Cobbinah <102822534+maamecobbinah@users.noreply.github.com>
2022-08-22 20:11:01 -0400

    Update Exercises.sql

--- Exercises.sql
+++ Exercises.sql
@@ -340,3 +340,24 @@ BEGIN
 		)	- 1
 END
 
+---Creating Stored Procedures
+CREATE PROCEDURE dbo.OrdersReport
+AS
+BEGIN
+	SELECT
+		*
+	FROM (
+		SELECT 
+			ProductName = B.[Name],
+			LineTotalSum = SUM(A.LineTotal),
+			LineTotalSumRank = DENSE_RANK() OVER(ORDER BY SUM(A.LineTotal) DESC)
+		FROM AdventureWorks2019.Sales.SalesOrderDetail A
+			JOIN AdventureWorks2019.Production.Product B
+				ON A.ProductID = B.ProductID
+		GROUP BY
+			B.[Name]
+		) X
+	WHERE LineTotalSumRank <= 10
+END
+--Execute stored procedure
+EXEC dbo.OrdersReport
